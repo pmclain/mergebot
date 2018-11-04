c829b05878d4b868fe031ac6233d44b1851c372d
Patrick McLain <pat@pmclain.com>
2018-11-04 11:17:56 -0500

    Configure automerge file whitelist from yaml

--- src/ActionHandler/Github/PullRequest/Opened/MergeCondition/WhitelistedFiles.php
+++ src/ActionHandler/Github/PullRequest/Opened/MergeCondition/WhitelistedFiles.php
@@ -2,32 +2,58 @@
 
 namespace App\ActionHandler\Github\PullRequest\Opened\MergeCondition;
 
+use App\Github\ConfigRepository;
 use App\Github\PullRequestManagement;
 
 class WhitelistedFiles implements MergeConditionInterface
 {
+    /* Example config yml
+        pullRequest:
+          opened:
+            autoMerge:
+              mergeCondition:
+                whitelistedFiles:
+                  allowedFiles:
+                    'composer.lock':
+                      - modified
+                    'yarn.lock':
+                      - modified
+                    'package.lock':
+                      - modified
+                    '.gitignore':
+                      - modified
+                      - added
+     */
+    const CONFIG_PATH_FILELIST = 'pullRequest/opened/autoMerge/mergeCondition/whitelistedFiles/allowedFiles';
     const PATTERN_NAME_ACTION = '%s_%s';
 
     /**
      * @var array
      */
-    private $filelist;
+    private $filelist = [];
 
     /**
      * @var PullRequestManagement
      */
     private $pullRequestManagement;
 
+    /**
+     * @var ConfigRepository
+     */
+    private $configRepository;
+
     public function __construct(
         PullRequestManagement $pullRequestManagement,
-        array $filelist = []
+        ConfigRepository $configRepository
     ) {
-        $this->filelist = $filelist;
         $this->pullRequestManagement = $pullRequestManagement;
+        $this->configRepository = $configRepository;
     }
 
     public function allowMerge(array $data): bool
     {
+        $this->initFilelist($data);
+
         if ($data['pull_request']['changed_files'] > count($this->filelist)) {
             return false;
         }
@@ -58,5 +84,16 @@ class WhitelistedFiles implements MergeConditionInterface
         }
 
         return $result;
+    }
+
+    private function initFilelist(array $data): void
+    {
+        $config = $this->configRepository->getConfig(
+            $data['pull_request']['base']['repo']['owner']['login'],
+            $data['pull_request']['base']['repo']['name'],
+            $data['pull_request']['base']['ref']
+        );
+
+        $this->filelist = $config->getValue(self::CONFIG_PATH_FILELIST) ?? [];
     }
 }
